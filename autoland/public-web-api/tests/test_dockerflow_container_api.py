911461305565692af9bafaa8395d5d0998e83bc1
Māris Fogels <mars@mozilla.com>
2017-02-07 15:59:39 -0500

    autoland: add integration tests for Dockerflow api calls (bug 1337501) r=smacleod
    Add integration tests for Ops' Dockerflow API calls.
    
    MozReview-Commit-ID: 8cpNjYXy8C3
    
    --HG--
    extra : rebase_source : c5f54decee00909763005401065fece8d4ba2b4c

---
+++ autoland/public-web-api/tests/test_dockerflow_container_api.py
@@ -0,0 +1,29 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+Tests to ensure we honour the Dockerflow container API specification.
+
+See https://github.com/mozilla-services/Dockerflow for details.
+"""
+
+import pytest
+
+# FIXME: need to get pycharm test runner to add /app to the container PYTHONPATH
+import sys
+sys.path.insert(0, '/app')
+
+from autolandweb.server import make_app
+
+
+@pytest.fixture
+def app():
+    return make_app(False)
+
+
+@pytest.mark.gen_test
+def test_loadbalancer_heartbeat_returns_200(http_client, base_url):
+    lb_heartbeat_url = base_url + '/__lbheartbeat__'
+    response = yield http_client.fetch(lb_heartbeat_url)
+    assert response.code == 200
