981bcdeb7afce8bde0b70d4cadfed8055393bddf
Steven MacLeod <smacleod@mozilla.com>
2017-03-21 11:42:50 -0400

    commit-index: stub out the iterations endpoint (Bug 1349219). r=dkl
    To get things working as quickly as possible we avoid any complex
    validation or logic in the iterations endpoint and just pass the
    data along to be turned into reviews. This change also lacks any
    persistence of the created iterations, always returning iteration
    id 1, and topic id 1.
    
    
    MozReview-Commit-ID: 482Df5xshmE
    
    --HG--
    extra : rebase_source : 1e2f71061cd6a103fde64322c56be26a84de71cc

--- commitindex/commitindex/api/iterations.py
+++ commitindex/commitindex/api/iterations.py
@@ -1,11 +1,26 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from commitindex.reviews.triggers import trigger_review
 
 
 def search():
     pass
 
 
-def post():
-    pass
+def post(commits):
+    # TODO: Validate the passed commits form a single linear DAG line.
+    # TODO: Create and persist a real iteration.
+
+    # Trigger review creation for this iteration.
+    trigger_review(commits)
+
+    return {
+        'data': {
+            'id': 1,
+            'topic': 1,
+            'commits': [{
+                'id': commit
+            } for commit in commits],
+        },
+    }, 200
