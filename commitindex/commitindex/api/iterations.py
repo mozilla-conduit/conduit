ce110ab4bb85276c1cbd41675abf07a01cdc3ff7
Steven MacLeod <smacleod@mozilla.com>
2017-03-21 13:06:21 -0400

    commit-index: make iterations/{id} always 404 (bug 1349250). r=davidwalsh,dkl
    Since we don't have actual persistent iterations yet we'll start
    this endpoint functionality with the 404 response.
    
    MozReview-Commit-ID: E772qCNP63a
    
    --HG--
    extra : rebase_source : c339cc8cd1b4e042ece0abd57fa51da94774dc6a

--- commitindex/commitindex/api/iterations.py
+++ commitindex/commitindex/api/iterations.py
@@ -1,11 +1,25 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from connexion import problem
+
 from commitindex.reviews.triggers import trigger_review
 
 
 def search():
     pass
+
+
+def get(id):
+    # TODO: Attempt to find a persisted iteration matching the request.
+
+    # We could not find a matching iteration.
+    return problem(
+        404,
+        'Iteration not found',
+        'The requested iteration does not exist',
+        type='https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404'
+    )
 
 
 def post(commits):
